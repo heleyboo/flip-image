4e77d996c32df5e98f9f5f23d1d5d42e28e6f077
Đỗ Văn Hoan <vanhoan90it6@gmail.com>
2023-11-09 12:03:06 +0700

    Update

--- Assets/Scripts/BackgroundTile.cs
+++ Assets/Scripts/BackgroundTile.cs
@@ -14,6 +14,8 @@ public class BackgroundTile : MonoBehaviour
 
     private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
 
+    private bool matchFound = false;
+
     void Awake() {
         Debug.Log("11111");
         render = GetComponent<SpriteRenderer>();
@@ -33,20 +35,97 @@ public class BackgroundTile : MonoBehaviour
         
     void OnMouseDown() {
         Debug.Log("324343434");
-        var x = BoardController.instance;
-        // 1
-        // if (render.sprite == null || BoardController.instance.IsShifting) {
-        //     return;
-        // }
-        //
-        // if (isSelected) { // 2 Is it already selected?
-        //     Deselect();
-        // } else {
-        //     if (previousSelected == null) { // 3 Is it the first tile selected?
-        //         Select();
-        //     } else {
-        //         previousSelected.Deselect(); // 4
-        //     }
-        // }
+        if (render.sprite == null || Board.instance.IsShifting) {
+            return;
+        }
+        
+        if (isSelected) { // 2 Is it already selected?
+            Deselect();
+        } else {
+            if (previousSelected == null) { // 3 Is it the first tile selected?
+                Select();
+            } else {
+                List<GameObject> allAdjacentTiles = GetAllAdjacentTiles();
+                if (allAdjacentTiles.Contains(previousSelected.gameObject)) { // 1
+                    SwapSprite(previousSelected.render); // 2
+                    previousSelected.ClearAllMatches();
+                    previousSelected.Deselect();
+                    ClearAllMatches();
+                } else { // 3
+                    previousSelected.GetComponent<BackgroundTile>().Deselect();
+                    Select();
+                }
+            }
+        }
     }
+    
+    public void SwapSprite(SpriteRenderer render2) { // 1
+        if (render.sprite == render2.sprite) { // 2
+            return;
+        }
+
+        Sprite tempSprite = render2.sprite; // 3
+        render2.sprite = render.sprite; // 4
+        render.sprite = tempSprite; // 5
+    }
+    
+    private GameObject GetAdjacent(Vector2 castDir) {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
+        if (hit.collider != null) {
+            return hit.collider.gameObject;
+        }
+        return null;
+    }
+    
+    private List<GameObject> GetAllAdjacentTiles() {
+        List<GameObject> adjacentTiles = new List<GameObject>();
+        for (int i = 0; i < adjacentDirections.Length; i++) {
+            adjacentTiles.Add(GetAdjacent(adjacentDirections[i]));
+        }
+        return adjacentTiles;
+    }
+    
+    private List<GameObject> FindMatch(Vector2 castDir) { // 1
+        List<GameObject> matchingTiles = new List<GameObject>(); // 2
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir); // 3
+        while (hit.collider != null && hit.collider.GetComponent<SpriteRenderer>().sprite == render.sprite) { // 4
+            matchingTiles.Add(hit.collider.gameObject);
+            hit = Physics2D.Raycast(hit.collider.transform.position, castDir);
+        }
+        return matchingTiles; // 5
+    }
+    
+    private void ClearMatch(Vector2[] paths) // 1
+    {
+        List<GameObject> matchingTiles = new List<GameObject>(); // 2
+        for (int i = 0; i < paths.Length; i++) // 3
+        {
+            matchingTiles.AddRange(FindMatch(paths[i]));
+        }
+        if (matchingTiles.Count >= 2) // 4
+        {
+            for (int i = 0; i < matchingTiles.Count; i++) // 5
+            {
+                matchingTiles[i].GetComponent<SpriteRenderer>().sprite = null;
+            }
+            matchFound = true; // 6
+        }
+    }
+    
+    public void ClearAllMatches() {
+        if (render.sprite == null)
+            return;
+
+        ClearMatch(new Vector2[2] { Vector2.left, Vector2.right });
+        ClearMatch(new Vector2[2] { Vector2.up, Vector2.down });
+        if (matchFound) {
+            render.sprite = null;
+            matchFound = false;
+            StopCoroutine(Board.instance.FindNullTiles());
+            StartCoroutine(Board.instance.FindNullTiles());
+        }
+    }
+
+
+
 }
--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -28,6 +28,9 @@ public class Board : MonoBehaviour
 
     private void Setup()
     {
+        Sprite[] previousLeft = new Sprite[height];
+        Sprite previousBelow = null;
+        
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
@@ -36,11 +39,83 @@ public class Board : MonoBehaviour
                 GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity);
                 backgroundTile.transform.parent = this.transform;
                 backgroundTile.name = string.Format("({0}, {1})", i, j);
-                Sprite newSprite = characters[Random.Range(0, characters.Count)]; // 2
+                
+                List<Sprite> possibleCharacters = new List<Sprite>(); // 1
+                possibleCharacters.AddRange(characters); // 2
+
+                possibleCharacters.Remove(previousLeft[j]); // 3
+                possibleCharacters.Remove(previousBelow);
+                
+                Sprite newSprite = possibleCharacters[Random.Range(0, possibleCharacters.Count)];
                 backgroundTile.GetComponent<SpriteRenderer>().sprite = newSprite; // 3
+                previousLeft[j] = newSprite;
+                previousBelow = newSprite;
             }
         }
     }
+    
+    public IEnumerator FindNullTiles() {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++)
+            {
+                SpriteRenderer render = allTiles[x, y].GetComponent<SpriteRenderer>();
+                if (render.sprite == null) {
+                    Debug.Log("Found null");
+                    yield return StartCoroutine(ShiftTilesDown(x, y));
+                    break;
+                }
+            }
+        }
+        
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                allTiles[x, y].GetComponent<BackgroundTile>().ClearAllMatches();
+            }
+        }
+    }
+    
+    private IEnumerator ShiftTilesDown(int x, int yStart, float shiftDelay = .03f) {
+        IsShifting = true;
+        List<SpriteRenderer>  renders = new List<SpriteRenderer>();
+        int nullCount = 0;
+
+        for (int y = yStart; y < width; y++) {  // 1
+            SpriteRenderer render = allTiles[x, y].GetComponent<SpriteRenderer>();
+            if (render.sprite == null) { // 2
+                nullCount++;
+            }
+            renders.Add(render);
+        }
+
+        for (int i = 0; i < nullCount; i++) { // 3
+            yield return new WaitForSeconds(shiftDelay);// 4
+            for (int k = 0; k < renders.Count - 1; k++) { // 5
+                renders[k].sprite = renders[k + 1].sprite;
+                renders[k + 1].sprite = GetNewSprite(x, height - 1);
+            }
+        }
+        IsShifting = false;
+    }
+    
+    private Sprite GetNewSprite(int x, int y) {
+        List<Sprite> possibleCharacters = new List<Sprite>();
+        possibleCharacters.AddRange(characters);
+
+        if (x > 0) {
+            possibleCharacters.Remove(allTiles[x - 1, y].GetComponent<SpriteRenderer>().sprite);
+        }
+        if (x < width - 1) {
+            possibleCharacters.Remove(allTiles[x + 1, y].GetComponent<SpriteRenderer>().sprite);
+        }
+        if (y > 0) {
+            possibleCharacters.Remove(allTiles[x, y - 1].GetComponent<SpriteRenderer>().sprite);
+        }
+
+        return possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+    }
+
+
+
 
     // Update is called once per frame
     void Update()
